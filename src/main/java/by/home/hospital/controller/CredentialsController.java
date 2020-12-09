4ae3e8a81ca98635353f144d22b24bf4ef9a744b
Evgeniy Slabysh <evgenii1900@gmail.com>
2020-12-09 16:47:01 +0300

    add timplates credentials

--- src/main/java/by/home/hospital/controller/CredentialsController.java
+++ src/main/java/by/home/hospital/controller/CredentialsController.java
@@ -5,28 +5,32 @@ import by.home.hospital.dto.PatientRegisterDto;
 import by.home.hospital.service.ICredentialsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-@RestController
+@Controller
 public class CredentialsController {
     @Autowired
-    private ICredentialsService repo;
+    private ICredentialsService credentialsService;
 
 
     //auth controller
     @PostMapping(path = "/patient/register" , consumes = MediaType.APPLICATION_JSON_VALUE)
     public void registerPatient(@RequestBody PatientRegisterDto patientRegisterDto) {
-        repo.registerPatient(patientRegisterDto);
+        credentialsService.registerPatient(patientRegisterDto);
     }
 
 
     @GetMapping("/credanchials")
-    public List<Credentials>  getCredentials() {
-        return repo.getCredentials();
+    public String  getCredentials(Model model) {
+        List <Credentials> credentials = credentialsService.getCredentials().stream().collect(Collectors.toList());
+        model.addAttribute("credentials",credentials);
+        return "credentialsList";
     }
 }
--- src/main/java/by/home/hospital/enums/Type.java
+++ src/main/java/by/home/hospital/enums/Type.java
@@ -4,4 +4,5 @@ public enum Type {
     PROCEDURE,
     DRUG,
     OPERATION;
+
 }
