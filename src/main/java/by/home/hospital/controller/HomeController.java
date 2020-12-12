253afbab867ac99d90ff7bc9f8a0c7571160fd0d
Evgeniy Slabysh <evgenii1900@gmail.com>
2020-12-12 18:32:52 +0300

    add homepage

---
+++ src/main/java/by/home/hospital/controller/HomeController.java
@@ -0,0 +1,17 @@
+package by.home.hospital.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String homePage(Model model) {
+        Date date = new Date();
+        model.addAttribute("date" ,date);
+        return "indexList";
+    }
+}
