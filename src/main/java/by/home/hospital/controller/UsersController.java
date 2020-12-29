2a37213a049fcc2fa29efc82c8c3f408723e3946
Evgeniy Slabysh <evgenii1900@gmail.com>
2020-12-29 14:05:57 +0300

    create view myaccount & controller

--- src/main/java/by/home/hospital/controller/UsersController.java
+++ src/main/java/by/home/hospital/controller/UsersController.java
@@ -2,9 +2,13 @@ package by.home.hospital.controller;
 
 
 import by.home.hospital.domain.User;
+import by.home.hospital.dto.MyViewDto;
 import by.home.hospital.service.IUserServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
@@ -12,8 +16,11 @@ import java.util.List;
 // todo CRUD methods
 @Controller
 public class UsersController {
+    @Autowired
+    private ConversionService conversionService;
 
     private final String INDEX = "index";
+    private final String VIEW = "myViewList";
 
     @Autowired
     private IUserServices repo;
@@ -33,5 +40,16 @@ public class UsersController {
         return repo.getUsers();
     }
 
+    @GetMapping("/myaccount")
+    public String getMyView(Authentication authentication, Model model) {
 
+        if (authentication == null) {
+            return "redirect:/login";
+        }
+        String login = authentication.getName();
+        MyViewDto view = (conversionService.convert(login, MyViewDto.class));
+        model.addAttribute("view", view);
+        model.addAttribute("login", login);
+        return this.VIEW;
+    }
 }
